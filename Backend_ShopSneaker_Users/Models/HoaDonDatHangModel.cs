fba404a6672aab60e1d9ee68fd33e915c466082b
Anhsngocha <ngocanhhy21@gmail.com>
2023-10-13 21:52:01 +0700

    Thêm model cho API cho người dùng

---
+++ Backend_ShopSneaker_Users/Models/HoaDonDatHangModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Models
+{
+    public class HoaDonDatHangModel
+    {
+        public int MaHoaDon { get; set; }
+        public DateTime NgayTao { get; set; }
+        public int MaVanChuyen { get; set; }
+        public int MaKhachHang { get; set; }
+        public string CodeHoaDon { get; set; }
+        public DateTime NgayDat { get; set; }
+        public int TrangThai { get; set; }
+
+    }
+    public class ChiTietHDModel
+    {
+        public int MaChiTietHD { get; set; }
+        public int MaHoaDon { get; set; }
+        public int MaSanPham { get; set; }
+        public int SoLuong { get; set; }
+        public int TongTien { get; set; }
+        public int MaGiamGia { get; set; }
+        public int MaPhiVC { get; set; }
+
+    }
+}
---
+++ Backend_ShopSneaker_Users/Models/SanPhamModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Models
+{
+    public class SanPhamModel
+    {
+        public int MaSanPham { get; set; }
+        public string TenSanPham { get; set; }
+
+        public int Size { get; set; }
+        public int GiaTien { get; set; }
+        public int GiamGia { get; set; }
+        public string LinkAnh { get; set; }
+        public string MoTa { get; set; }
+        public int SoLuong { get; set; }
+        public int DaBan { get; set; }
+        public int MaDanhMuc { get; set; }
+        public int MaThuongHieu { get; set; }
+
+    }
+}
